7e40a8e0f5041a31295739fd6b87c4c1578816d5
ggbandit <ggbandit@hotmail.com>
2018-05-08 03:32:39 +0700

    project for final

--- src/App.js
+++ src/App.js
@@ -50,7 +50,6 @@ class App extends Component {
         userselect.map(data => {
             return this.setState({id: data.id, name: data.Name, faculty: data.Faculty, year: data.Year})
         });
-
     };
     handleUpdate = (e) => {
         const {name, faculty, year, id} = this.state;
@@ -66,6 +65,8 @@ class App extends Component {
 
     render() {
         const {userlist} = this.props.users;
+        console.log(userlist);
+        console.log('hello');
         const {Name, Faculty, Year, name, faculty, year} = this.state;
         const {
             handleSubmit,
@@ -75,24 +76,11 @@ class App extends Component {
             handleUpdate
         } = this;
         return (
-            <div className="App">
+            <div>
                 <Header/>
                 <Tester Name={Name} Faculty={Faculty} Year={Year}/>
                 <br/>
-                <h1>User in system</h1>
-                <ul>
-                    {
-                        userlist.map(data => {
-                            if (data != null) {
-                                return <li
-                                    key={data.id}>{"ID:" + data.id + " Name:" + data.Name + " " + data.Faculty + " Year: " + data.Year}
-                                    <button id={data.id} onClick={handletoEditform}>Edit</button>
-                                    <button id={data.id} onClick={handleDelete}>Delete</button>
-                                </li>
-                            }
-                        })
-                    }
-                </ul>
+                <div className="center">
                 <h1>ADD USER</h1>
                 <label htmlFor="">Name</label>
                 <input type="text" name="Name" onChange={handleChange} value={Name}/>
@@ -109,6 +97,19 @@ class App extends Component {
                 <label htmlFor="">Year</label>
                 <input type="text" name="year" onChange={handleChange} value={year}/>
                 <button onClick={handleUpdate}>Update</button>
+                <h1>User in database</h1>
+                    {
+                        userlist.map((data) => {
+                            if (data != null) {
+                                return <li key={data.id}>
+                                {"ID:" + data.id + " Name: " + data.Name +" "+"Faculty: " + data.Faculty + " Year: " + data.Year}
+                                    <button id={data.id} onClick={handletoEditform}>Edit</button>
+                                    <button id={data.id} onClick={handleDelete}>Delete</button>
+                                </li>
+                            }
+                        })
+                    }
+                </div>
             </div>
         );
     }
@@ -128,4 +129,5 @@ const mapStateToProps = ({users}) => {
         users,
     }
 }
+
 export default connect(mapStateToProps, {changeUser, fetchUser, createUser, deleteUser, updateUser})(App);
